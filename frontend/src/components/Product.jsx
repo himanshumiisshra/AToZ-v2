60f682901efbe482b33debdf8d2a48264dfa3398
Himanshu Mishra <star.anshm@gmail.com>
2023-10-21 02:32:25 +0530

    product Screen

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,29 +1,34 @@
-import { Card } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Rating from "./Rating";
 
-import React from 'react'
+import React from "react";
 
-const Product = ({product}) => {
+const Product = ({ product }) => {
   return (
-    <Card className='my-3 p-3 rounded'>
-        <Link to={`/product/${product._id}`}>
-            <Card.Img src={product.image} variant='top' />     
-         </Link>
+    <Card className="my-3 p-3 rounded">
+      <Link to={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant="top" />
+      </Link>
 
-    <Card.Body>
+      <Card.Body>
         <Link to={`/product/${product._id}`}>
-            <Card.Title as="div">
-                <strong>{product.name}</strong>
-            </Card.Title>
+          <Card.Title as="div" className="product-title">
+            <strong>{product.name}</strong>
+          </Card.Title>
         </Link>
 
-        <Card.Text as="h3">
-            ${product.price}
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
         </Card.Text>
-    </Card.Body>
 
+        <Card.Text as="h3">${product.price}</Card.Text>
+      </Card.Body>
     </Card>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
