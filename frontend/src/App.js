b80076700f79ee5998ee4d535e17b05bfe0ef489
Himanshu Mishra <star.anshm@gmail.com>
2023-10-20 19:25:20 +0530

    second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer";
+
 
 const App = () => {
   return (
-    <h1>Welcome to AToZ</h1>
-  )
-}
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+         <Outlet/>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
